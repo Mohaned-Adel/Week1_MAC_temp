4bcfbbf356717397db8296f6c7b28a76c400bfb5
Mohaned Adel <mohaned.adel1996@gmail.com>
2022-06-01 15:24:05 +0200

    modified: navBar scrolling event

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 var dropicon = document.getElementById('drop-bt');
 let navList = document.getElementById('nav-list');
+let navBar = document.querySelector('#nav-bar');
 let closeTag = document.querySelector('.close');
 
 dropicon.addEventListener('click', function() {
@@ -10,3 +11,7 @@ dropicon.addEventListener('click', function() {
 closeTag.addEventListener('click', function() {
     navList.classList.toggle('active');
 });
+
+window.addEventListener('scroll', function(){
+    navBar.classList.toggle('scrolling-active', window.scrollY > 0 );
+})
